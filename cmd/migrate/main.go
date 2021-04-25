067097f3aa13f93836e778d5068906c653e7fee1
Charles Harries <charles@harries.me>
2021-04-25 10:54:53 +0100

    Migrate notes table

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -18,6 +18,13 @@ var userStatement = `create table if not exists "users" (
 	"active" boolean not null check (active in (0, 1)) default 0
 );`
 
+var notesStatement = `create table if not exists "notes" (
+	"id" integer not null primary key autoincrement,
+	"date" datetime not null default "",
+	"updated_at" datetime null,
+	"content" text not null default "[]"
+);`
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -30,6 +37,11 @@ func main() {
 	}
 
 	_, err = db.Exec(userStatement)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(notesStatement)
 	if err != nil {
 		log.Fatal(err)
 	}

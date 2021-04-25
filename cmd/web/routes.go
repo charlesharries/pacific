c144ba2455302b5dc6bf1907ad73cec6455ecdf5
Charles Harries <charles@harries.me>
2021-04-25 14:36:16 +0100

    Restructure routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,24 +7,53 @@ import (
 	"github.com/justinas/alice"
 )
 
+// middlewareGroup allows us to group a bunch of routes together under
+// a chain of middleware.
+type middlewareGroup struct {
+	mux        *pat.PatternServeMux
+	middleware alice.Chain
+}
+
+// middlewareGroupHandler is the callback passed to the middleware
+// group.
+type middlewareGroupHandler func(*middlewareGroup)
+
+// Get passes any GET requests to this group on to the group's router.
+func (c *middlewareGroup) Get(route string, h func(w http.ResponseWriter, r *http.Request)) {
+	c.mux.Get(route, c.middleware.ThenFunc(h))
+}
+
+// Post passes any POST requests to this group on to the group's router.
+func (c *middlewareGroup) Post(route string, h func(w http.ResponseWriter, r *http.Request)) {
+	c.mux.Post(route, c.middleware.ThenFunc(h))
+}
+
+// newMiddlewareGroup creates a new middleware group.
+func (app *application) newMiddlewareGroup(mux *pat.PatternServeMux, middleware alice.Chain, fn middlewareGroupHandler) {
+	r := &middlewareGroup{mux, middleware}
+
+	fn(r)
+}
+
+// routes declares our application routes.
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
 	dynamicMiddleware := alice.New(app.session.Enable, noSurf, app.authenticate)
 
 	mux := pat.New()
 
-	mux.Get("/", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.home)))
+	app.newMiddlewareGroup(mux, dynamicMiddleware, func(r *middlewareGroup) {
+		r.Get("/", app.home)
+		r.Get("/register", app.registerForm)
+		r.Post("/register", app.register)
+		r.Get("/login", app.loginForm)
+		r.Post("/login", app.login)
+		r.Post("/logout", app.logout)
 
-	// Auth routes
-	mux.Get("/register", dynamicMiddleware.ThenFunc(app.registerForm))
-	mux.Post("/register", dynamicMiddleware.ThenFunc(app.register))
-	mux.Get("/login", dynamicMiddleware.ThenFunc(app.loginForm))
-	mux.Post("/login", dynamicMiddleware.ThenFunc(app.login))
-	mux.Post("/logout", dynamicMiddleware.ThenFunc(app.logout))
-
-	// Note routes
-	mux.Post("/notes/:date", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.updateNote)))
+		// Note routes
+		r.Get("/notes/:date", app.getNote)
+		r.Post("/notes/:date", app.updateNote)
+	})
 
 	fileServer := http.FileServer(http.Dir("./public"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))

954b149018e254873b14441bf971dce89fa7d46e
Charles Harries <charles@harries.me>
2021-05-01 20:25:00 +0100

    Support the back button

--- resources/js/hooks/useHistory.ts
+++ resources/js/hooks/useHistory.ts
@@ -1,15 +1,34 @@
-import { useEffect } from 'react';
-import { dateString, useDate } from '../lib/date';
+import { useCallback, useEffect } from 'react';
+import { dateString, useDate, dateFromUrl } from '../lib/date';
 
 export default function useHistory(): void {
-  const { current } = useDate();
+  const { current, setCurrent, setViewing } = useDate();
+
+  const handlePopState = useCallback(() => {
+    setCurrent(dateFromUrl());
+    setViewing(dateFromUrl());
+  }, [setCurrent, setViewing]);
+
+  useEffect(() => {
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [handlePopState]);
 
   useEffect(() => {
     const date = dateString(current);
     const url = new URL(window.location.toString());
 
-    url.pathname = date;
+    // If we've just hit the back button, current will be the same as
+    // the URL for some reason--so don't push that change back into
+    // window.history.
+    if (`/${date}` === url.pathname) {
+      return;
+    }
 
-    window.history.pushState({}, '', url.toString());
+    url.pathname = date;
+    window.history.pushState({ url: date }, '', url.toString());
   }, [current]);
 }
